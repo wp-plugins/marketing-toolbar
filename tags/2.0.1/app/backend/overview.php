63efbfa1b2569656faaa906327fa0b50fb6f3b1e
nature_kp <nature_kp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-23 16:23:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/marketing-toolbar@303358 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/2.0.1/app/backend/overview.php
+++ tags/2.0.1/app/backend/overview.php
@@ -40,3 +40,41 @@ global $wpdb;
     </tbody>
   </table>
 </div>
+<div>
+<?php
+if($_POST['update']=='yes')
+{
+@mysql_query("ALTER TABLE `".$wpdb->rspro."` ADD `product_link` varchar(80) NOT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `product_text` varchar(80) NOT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `product_color` varchar(20) NOT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `total_hits` int(10) DEFAULT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `vibration_time` int(10) DEFAULT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `vibration_number` int(10) DEFAULT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `vibration_count` int(10) DEFAULT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `auto_vibration` varchar(50) NOT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `display_close` varchar(80) NOT NULL");
+@mysql_query("ALTER TABLE `".$wpdb->rspro."`ADD `price` varchar(80) NOT NULL");	
+}
+?>
+<?php
+$raw=array();
+$result = mysql_query("SHOW FIELDS FROM $wpdb->rspro");
+$i = 0;
+while ($row = mysql_fetch_array($result)) {
+$raw[]=$row['Field'];
+}
+if(!in_array('vibration_count',$raw))
+echo "<br><br><br><br>
+Dear User,<br>
+<b>Your are using <font color='red'>old version database</font> of Wordpress marketing toolbar Plugin. New Version May not work properly with old databse so Please Update your database by clicking in \"Update My Database\" Button Below.\n
+Thanks!</b><br><br>".'
+<form name="check" method="post">
+<input type="hidden" name="update" value="yes" />
+<input type="submit" value="Update My Database"  />
+</form>
+';
+
+?>
+
+
+</div>
